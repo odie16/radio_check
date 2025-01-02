7da1421589b9bd89f5b467a5e6d632f1ed99bab5
odie16 <dobisik.ondrej@gmail.com>
2025-01-02 23:08:14 +0100

    add to gitignore

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 radio_check.sh
 
-A bash script that checks urls saved in radiotray-ng bookmarks using ffprobe. It can simultaneously play them for a specified # of seconds with mpv (media player).
+A bash script that checks urls saved in [radiotray-ng](https://github.com/ebruck/radiotray-ng) bookmarks using ffprobe (part of [FFmpeg](https://github.com/FFmpeg/FFmpeg)). It can simultaneously play them for a specified # of seconds with mpv (media player).
 
-Included is my current bookmarks.json for radiotray-ng.
+Included is my current bookmarks.json file for use in radiotray-ng.
--- radio_check.sh
+++ radio_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Můj první trošku složitější skript! Začátek března 2023.
+# v0.1 - můj první trošku složitější skript! Začátek března 2023.
 # v0.2 - opraveno pomocí ChatGPT v.3 :D
-# v0.3 - dáno na GitHub
+# -> v0.3 - různá vylepšní, opravy, dáno na GitHub…
 # 
 # Kontroluje, jestli rádia, které mám uložené v záložkách radiotray-ng, jsou stále online.
 # Až to budeš za pár let číst, budoucí Odie, odpusť mi, vždyť nevím, co činím.
@@ -12,8 +12,11 @@
 # TODO
 # -[] Ve fci CheckRadios: uložit $name a $url do array a udělat cyklus
 # -[] Ukládat stanice podle zemí.
-# -[] Možnost použití jiného přehrávače než mpv.
+# -[] Možnost použití jiného přehrávače než mpv (a zkontrolovat/nabídnout možnost jejich použití; a pokud se třeba nenajde mpv, tak ho vůbec nenabízet)
 # -[] Automatická oprava v záložkách
+# -[] Přidat možnost ladění - přepínač, který způsobí, že se stanice jenoom vypíšou a neuloží.
+# -[] Doplnit nutné závislosti (bash, radiotray), tak volitelné (radiotray-ng, mpv)
+# -[] Automatické a interaktivní nastavení proměnných; uložit do samostatného souboru?
 # DONE
 # -[x] Při použití platného přepínače spustit skript neinteraktivně.
 
@@ -30,7 +33,7 @@ path_to_bookmarks=$HOME/.config/radiotray-ng/bookmarks.json # cesta k záložká
 
 # nápověda
 function Help {
-  echo "'radio_check2.sh' HELP PAGE"
+  echo "'radio_check.sh' MAN PAGE"
   echo
   echo "This script checks urls saved in radiotray-ng bookmarks using ffprobe. It can simultaneously play them for a specified # of seconds with mpv (media player)."
   echo
